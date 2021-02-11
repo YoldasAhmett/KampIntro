99165672b41af4204b4588b934ec032de291fd7f
Ahmet Yoldaş <theahmet4430@gmail.com>
2021-02-11 13:04:49 +0300

    Sql ile veriyi sorgulama 6.Hafta

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,43 +1,77 @@
 ﻿--Select
---ANSII
-Select ContactName Adi,CompanyName Sirketadi,City Sehir from Customers
+Select ContactName BağlantıAdı,CompanyName ŞirketAdı,City Şehir from Customers
 
 Select * from Customers where City = 'Berlin'
 
---case insensitive
-sElEct * from Products where categoryId = 1 and UnitPrice >= 10
+--case insensitive = Büyük küçük harf duyarsız
+select * from products where CategoryID = 1
 
-sElect * from Products where categoryId = 1 or categoryId = 3
+Select * from products where CategoryId = 1 or CategoryID = 3
 
-select * from Products where CategoryID = 1 order by UnitPrice desc 
---asc ascending //artan sırada sıralama
---desc descending //azalan sırada sıralama
+select * from products where CategoryID = 1 and UnitPrice >= 10
 
-Select count(*) Adet from Products where CategoryID = 2
+select * from Products order by CategoryID
 
+select * from Products order by CategoryID, ProductID 
 
-select CategoryID,count(*) from Products where UnitPrice>20
-group by CategoryID having count(*)<10
+select * from Products order by CategoryID asc --ascending = artan sırada
+--Bu komut 2 üstteki komut ile aynı şey çünkü default olarak sıralamadan dolayı
 
+select * from Products order by CategoryID desc --descending = azalan sırada
 
---join: birleştirmek
+select * from Products where SupplierID = 1 order by UnitPrice desc
 
-select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
+Select count(*) from Products
+
+Select count(*) Adet from Products where CategoryID = 3
+
+Select count(*) Adet from Products where CategoryID = 3
+
+select CategoryID from Products group by CategoryID
+
+select CategoryID, UnitPrice from Products group by CategoryID, UnitPrice
+
+select count(*) from Products group by CategoryID
+
+select CategoryID, UnitPrice, count(*) from Products group by CategoryID,UnitPrice
+
+select CategoryId, count(*) From Products where UnitPrice > 20 group by CategoryID 
+
+select CategoryId, count(*) Sayısı From Products where UnitPrice > 20
+group by CategoryID having count(*) < 5
+
+select * from Products inner join Categories --inner join: İki tabloda da
+on Products.CategoryID = Categories.CategoryID --ortak olanları getir.
+
+select Products.ProductID, Products.ProductName,
+Products.UnitPrice, Categories.CategoryName
 from Products inner join Categories
 on Products.CategoryID = Categories.CategoryID
-where Products.UnitPrice>10
 
---DTO: Data Transformation Object
+select Products.ProductID, Products.ProductName,
+Products.UnitPrice, Categories.CategoryName
+from Products inner join Categories
+on Products.CategoryID = Categories.CategoryID
+where Products.UnitPrice > 10
 
-Select * from Products p left join [Order Details] od
+select * from Products p inner join [Order Details] od 
 on p.ProductID = od.ProductID
+
+select * from Products p inner join [Order Details] od 
+on p.ProductID = od.ProductID inner join Orders o 
+on o.OrderID = od.OrderID -- Üç tabloyu inner join ettik.
+
+
+select * from Products p left join [Order Details] od --left join:inner join +  
+on p.ProductID = od.ProductID --solda olup sağda olmayanları da getir.
+
+Select * from Customers c left join Orders o
+on c.CustomerID = o.CustomerID
 
 Select * from Customers c left join Orders o
 on c.CustomerID = o.CustomerID
 where o.CustomerID is null
 
-
-
-
-
-
+Select * from Customers c right join Orders o --right joinde inner join + 
+on c.CustomerID = o.CustomerID              --solda olup sağda olmayanları da getir.
+where o.CustomerID is null
