0620249becae054b327dbda62f4b05696f1ba1f0
Ahmet Yoldaş <theahmet4430@gmail.com>
2021-02-12 16:47:14 +0300

    Sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿Select Products.ProductName [Ürün Adı],
+Sum([Order Details].Quantity * [Order Details].UnitPrice) [Kazanılan Toplam Miktar]
+from Products inner join [Order Details]
+On Products.ProductID = [Order Details].ProductID inner join Orders 
+On [Order Details].OrderID = Orders.OrderID
+group by Products.ProductName
+Order by Sum([Order Details].Quantity * [Order Details].UnitPrice) desc
